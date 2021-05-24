e17a458c4539793565bb0fe24083bfe4b64cf4fd
newForinux <you_maybe@naver.com>
2021-05-25 01:09:42 +0900

    Modify the login button to suit Google policy.

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'appState/app_state.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -10,34 +11,94 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ApplicationState>(
-        builder: (context, user, child) {
-          return Scaffold(
-            body: SafeArea (
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget> [
-                  Column(
-                    mainAxisAlignment:  MainAxisAlignment.center,
-                    children: <Widget> [
-                      Text('심플'),
-                      SizedBox(height:10),
-                      RaisedButton(
-                          child:Text('anonymous log in'),
-                          onPressed: () async {
-                            Provider.of<ApplicationState>(context, listen: false).signInAnonymously();
-                            await Navigator.popAndPushNamed(context, '/home');
-                          }
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80),
+            Column(
+              children: [
+                Text('심플')
+              ],
             ),
-          );
-        });
+            ButtonTheme(
+              height: 40,
+              padding: EdgeInsets.all(0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(3.0),
+                side: BorderSide.none
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  signInWithGoogle().then((UserCredential userCredential) {
+                    Navigator.pop(context);
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(1.0),
+                      child: Container(
+                        height: 38.0,
+                        width: 38.0,
+                        decoration: BoxDecoration(
+                          color: null,
+                          borderRadius: BorderRadius.circular(3.0),
+                        ),
+                        child: Center(
+                          child: Image.network(
+                            'https://pngimg.com/uploads/google/google_PNG19635.png',
+                            fit: BoxFit.cover,
+                            height: 30.0,
+                            width: 30.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 14.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
+                      child: Text(
+                        "구글 아이디로 로그인",
+                        style: GoogleFonts.notoSans(
+                          textStyle: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black.withOpacity(0.54),
+                          )
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
