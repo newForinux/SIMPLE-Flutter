c5589750728b39f42b3898b78d72fc71ffd16363
newForinux <you_maybe@naver.com>
2021-06-04 20:25:54 +0900

    Add image picker and related dependencies.

--- lib/add.dart
+++ lib/add.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:simple_flutter/app.dart';
 
 class AddPage extends StatefulWidget {
   @override
@@ -12,50 +18,74 @@ class _AddPageState extends State<AddPage> {
 
   final CollectionReference errands = FirebaseFirestore.instance.collection('errands');
   var user = FirebaseAuth.instance.currentUser;
+  FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+  String _productImageUrl = "";
 
-  List<String> _valueList = [
-    '기타', '편의점', '커피&디저트', '중고거래대행', '약국', '택배&우편물',
-    '세탁소&구두', '상품교환', '짐옮기기',
-  ];
-  var _selectedCategory = '기타';
 
+  late File _image;
+  final picker = ImagePicker();
   final _titleController = TextEditingController();
-  final _titleFormkey = GlobalKey<FormState>();
+  final _titleFormKey = GlobalKey<FormState>();
   final _rewardController = TextEditingController();
   final _descriptionController = TextEditingController();
 
-  Future<void> addCard() {
-    return errands.doc(user!.uid).set({
-      'category':_selectedCategory,
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print("No Image selected.");
+      }
+    });
+  }
+
+
+  Future<void> addCard(String category) {
+    if (category.compareTo("카페/디저트") == 0)
+      category = "카페";
+    else if (category.compareTo("세탁/클리닝") == 0)
+      category = "세탁";
+
+    return FirebaseFirestore.instance.collection(category).add({
+      'category': category,
       'title': _titleController.text,
       'description':_descriptionController.text,
-      'reward': (_rewardController.text.isEmpty)? 0 : int.tryParse(_rewardController.text),
+      'reward': (_rewardController.text.isEmpty)? 1000 : int.tryParse(_rewardController.text),
       'userId': user!.uid,
       'timestamp': FieldValue.serverTimestamp(),
+      'ongoing_list': [],
+      'image': _productImageUrl,
       'ongoing': false,
       'done': false,
     })
-        .then((value) => print("Added"))
+        .then((value) => Navigator.pop(context))
         .catchError((error) => print("Failed to Add: $error"));
   }
 
   @override
   Widget build(BuildContext context) {
+    final argument = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.cancel),
-          onPressed: () => Navigator.pushNamed(context, '/home'),
+          onPressed: () => Navigator.pop(context),
         ),
-        title: Text('Add'),
+        title: Text('심부름 등록'),
         actions: [
           TextButton(
-            child: Text('SAVE'),
+            child: Text(
+              '등록',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             onPressed: () async {
-              if (_titleFormkey.currentState!.validate()) {
-                await addCard();
-                Navigator.pushNamed(context, '/home');
+              if (_titleFormKey.currentState!.validate()) {
+                await addCard(argument);
               } else {
                 ScaffoldMessenger.of(context)
                     .showSnackBar(SnackBar(content: Text('Processing Data'),));
@@ -68,79 +98,137 @@ class _AddPageState extends State<AddPage> {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
-            Text('카테고리', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
-            SizedBox(height:8),
-            Text('종류를 선택하세요', style: TextStyle(color: Colors.grey)),
-            DropdownButtonHideUnderline(
-              child: DropdownButton(
-                value: _selectedCategory,
-                items: _valueList.map(
-                    (value) {
-                      return DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      );
-                    },
-                ).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedCategory= value.toString();
-                  });
-                },
-              ),
+            Row(
+              children: [
+                Text(
+                  '카테고리',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+                ),
+                SizedBox(width: 15),
+                Text(
+                  argument,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Vitro Pride',
+                  ),
+                )
+                // DropdownButtonHideUnderline(
+                //   child: DropdownButton(
+                //     value: _selectedCategory,
+                //     items: _errandsList.map((value) {
+                //         return DropdownMenuItem(
+                //           value: value,
+                //           child: Text(value),
+                //         );
+                //       },
+                //     ).toList(),
+                //     onChanged: (value) {
+                //       setState(() {
+                //         _selectedCategory= value.toString();
+                //       });
+                //     },
+                //   ),
+                // ),
+              ],
             ),
-            SizedBox(height:8),
-            Divider(
-              height: 8,
-              thickness: 1,
-              color: Colors.grey,
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Text(
+                    '지급 비용',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)
+                ),
+                SizedBox(width: 15),
+                Flexible(
+                  child: Container(
+                    width: 100,
+                    child: TextField(
+                      style: TextStyle(
+                        color: Color(PURPLE),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      controller: _rewardController,
+                      decoration: InputDecoration(
+                        hintText: '1000',
+                        hintStyle: TextStyle(
+                          color: Color(PURPLE),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        )
+                      ),
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                      '  원',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+
+                    ),
+                  ),
+                )
+              ],
             ),
-            SizedBox(height:8),
+            SizedBox(height: 30),
             Form(
-              key: _titleFormkey,
+              key: _titleFormKey,
               child: TextFormField(
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                ),
                 controller: _titleController,
                 decoration: InputDecoration(
-                  hintText: '제목: ',
+                  hintText: '무엇이 필요하신가요?',
                   hintStyle: TextStyle(fontWeight: FontWeight.bold,),
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return '필수항목입니다';
+                    return '필수 항목입니다.';
                   }
                   return null;
                 },
               ),
             ),
+            SizedBox(height: 16),
+            SizedBox(
+              child: ElevatedButton(
+                onPressed: getImage,
+                child: Icon(
+                  Icons.camera_alt_outlined,
+                  color: Colors.black,
+                ),
+                style: ElevatedButton.styleFrom(
+                    side: BorderSide(color: Colors.black, width: 2.0),
+                    padding: EdgeInsets.all(10.0),
+                    primary: Colors.white
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
             Container(
               height: 300,
+              color: Colors.grey[200],
               child: TextField(
+                style: TextStyle(
+                    fontSize: 16
+                ),
                 controller: _descriptionController,
                 decoration: InputDecoration(
-                  hintText: '내용을 입력하세요: (시간, 장소, 진행 방식 등)',
+                  hintText: '심부름을 위한 자세한 내용이 필요해요!\n(시간, 장소, 진행 방식 등)',
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
                 ),
                 maxLines: null,
               ),
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: TextField(
-                    controller: _rewardController,
-                    decoration: InputDecoration(
-                      hintText: '심부름 값:',
-                    ),
-                    textAlign: TextAlign.right,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly,
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Text('  원'),
-                )
-              ],
             ),
           ],
         ),
